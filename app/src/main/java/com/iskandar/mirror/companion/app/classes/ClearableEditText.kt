5a1c03e5e29475d3cdf02ec95c47e2c6bb24d829
Bangel25 <claytp25@gmail.com>
2020-12-09 15:37:09 -0500

    IMA-001A: detekt gradle task fix

--- app/src/main/java/com/iskandar/mirror/companion/app/classes/ClearableEditText.kt
+++ app/src/main/java/com/iskandar/mirror/companion/app/classes/ClearableEditText.kt
@@ -11,9 +11,11 @@ fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
     this.addTextChangedListener(
         object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // Comment needed for gradle task detekt to pass
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // Comment needed for gradle task detekt to pass
             }
 
             override fun afterTextChanged(editable: Editable?) {
