0870253055cb72d83dd738b0982b8a2ef4992299
Bangel25 <claytp25@gmail.com>
2021-02-02 18:35:03 -0500

    IMA-007: HomeActivity.kt populated with tile buttons that go to their respective activities

--- app/src/main/java/com/iskandar/mirror/companion/app/activities/ui/HomeActivity.kt
+++ app/src/main/java/com/iskandar/mirror/companion/app/activities/ui/HomeActivity.kt
@@ -1,7 +1,10 @@
 package com.iskandar.mirror.companion.app.activities.ui
 
+import android.content.Intent
 import android.os.Bundle
 import com.iskandar.mirror.companion.app.R
+import com.iskandar.mirror.companion.app.activities.ui.alarms.AlarmOverviewActivity
+import com.iskandar.mirror.companion.app.activities.ui.reminders.RemindersOverviewActivity
 import com.iskandar.mirror.companion.app.classes.BaseActivity
 import kotlinx.android.synthetic.main.activity_home.*
 
@@ -12,5 +15,23 @@ class HomeActivity : BaseActivity() {
         setUpNavigationBar()
         // Highlights the item in the navigation bar
         nav_view.setCheckedItem(R.id.nav_home)
+
+        // Set up listeners for tile buttons
+        alarms_button.setOnClickListener {
+            val intent = Intent(this, AlarmOverviewActivity::class.java)
+            startActivity(intent)
+        }
+        reminders_button.setOnClickListener {
+            val intent = Intent(this, RemindersOverviewActivity::class.java)
+            startActivity(intent)
+        }
+        background_button.setOnClickListener {
+            val intent = Intent(this, BackgroundOverviewActivity::class.java)
+            startActivity(intent)
+        }
+        change_information_button.setOnClickListener {
+            val intent = Intent(this, ChangeInformationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
